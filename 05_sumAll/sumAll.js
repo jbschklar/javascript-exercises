b64051d92867690031ee0752d4202e45de27a200
jbschklar <jbschklar@gmail.com>
2022-05-10 15:54:07 -0400

    finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (firstNum, lastNum) {
+	let sum = 0;
+	if (
+		firstNum < 0 ||
+		lastNum < 0 ||
+		typeof firstNum !== "number" ||
+		typeof lastNum !== "number"
+	)
+		return "ERROR";
+	x = firstNum < lastNum ? firstNum : lastNum;
+	y = lastNum > firstNum ? lastNum : firstNum;
 
+	for (let i = x; i <= y; i++) {
+		sum = sum + i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
