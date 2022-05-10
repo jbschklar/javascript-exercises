c45eed34e205c120ec7730c73d4f5a76abbd07d7
jbschklar <jbschklar@gmail.com>
2022-05-10 12:37:16 -0400

    finished exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let outPut = [];
+	if (num < 0) return "ERROR";
+	for (let i = 0; i < num; i++) {
+		outPut.push(string);
+	}
+	return outPut.join("");
 };
 
+// console.log(repeatString("hey", 10));
 // Do not edit below this line
 module.exports = repeatString;
