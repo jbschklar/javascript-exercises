ce1c0e94f71066d8216f794ae0d9d06558c3f230
jbschklar <jbschklar@gmail.com>
2022-05-10 15:27:10 -0400

    completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+	let arr = arguments[0];
+	let num = [];
+	for (let i = 1; i < arguments.length; i++) {
+		num.push(arguments[i]);
+	}
+	num.forEach((num) => {
+		arr.forEach((el) => {
+			if (el === num) arr.splice(arr.indexOf(el), 1);
+			console.log(el);
+		});
+	});
 
+	return arr;
 };
+
+// console.log(removeFromArray([1, 2, 3, 4, 5], 3, 4, 5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
