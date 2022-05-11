e39aa3cfe727702cad2ee44e819eddb055424d3a
jbschklar <jbschklar@gmail.com>
2022-05-11 14:55:40 -0400

    finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (degF) {
+	const degC = Math.round((degF - 32) * (5 / 9) * 10) / 10;
+	return degC;
 };
 
-const ctof = function() {
-
+const ctof = function (degC) {
+	const degF = Math.round((degC * (9 / 5) + 32) * 10) / 10;
+	return degF;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
