ee7b4b0b92010d5133eef008cdc72446060b8bf1
jbschklar <jbschklar@gmail.com>
2022-05-24 15:57:25 -0400

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const newStr = str.toLowerCase().replace(/[ ,.!]/g, "");
+	const reverseStr = newStr.split("").reverse().join("");
 
+	return newStr === reverseStr ? true : false;
 };
 
 // Do not edit below this line
