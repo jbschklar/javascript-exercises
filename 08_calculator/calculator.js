3fb513a653aa6ccf11d0f42b533401af5336e8e5
jbschklar <jbschklar@gmail.com>
2022-05-24 15:09:18 -0400

    finished calulator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function () {
+	return Array.from(arguments).reduce((total, argument) => {
+		total = total + argument;
+		return total;
+	}, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
-};
+// const factorial = function (num) {
+// 	if (num < 0) return -1;
+// 	if (num === 0 || num === 1) return 1;
+// 	return num * factorial(num - 1);
+// };
 
+const factorial = function (num) {
+	let sum = 1;
+	for (let i = num; i > 0; i--) {
+		sum = sum * i;
+	}
+	return sum;
+};
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
