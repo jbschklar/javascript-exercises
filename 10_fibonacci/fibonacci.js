894f4efb3a3e85d824b0f207cb2bb9bc6a4e5e6a
jbschklar <jbschklar@gmail.com>
2022-05-25 12:59:30 -0400

    refactored fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,11 @@
 const fibonacci = function (num) {
 	if (+num < 0) return "OOPS";
 	let fibArr = [];
+	let a, b;
 	for (let i = 0; i <= +num; i++) {
-		fibArr.push(
-			(fibArr[i - 1] ? fibArr[i - 1] : 1) + (fibArr[i - 2] ? fibArr[i - 2] : 0)
-		);
-		console.log(fibArr[i - 1] + fibArr[i - 2]);
+		a = fibArr[i - 1];
+		b = fibArr[i - 2];
+		fibArr.push((a ? a : 1) + (b ? b : 0));
 	}
 	return fibArr[+num - 1];
 };
