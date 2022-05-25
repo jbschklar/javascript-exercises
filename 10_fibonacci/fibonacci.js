fdf68802c83f99657991f5e3e7f2b09ac92b7f90
jbschklar <jbschklar@gmail.com>
2022-05-25 12:54:42 -0400

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	if (+num < 0) return "OOPS";
+	let fibArr = [];
+	for (let i = 0; i <= +num; i++) {
+		fibArr.push(
+			(fibArr[i - 1] ? fibArr[i - 1] : 1) + (fibArr[i - 2] ? fibArr[i - 2] : 0)
+		);
+		console.log(fibArr[i - 1] + fibArr[i - 2]);
+	}
+	return fibArr[+num - 1];
 };
 
 // Do not edit below this line
